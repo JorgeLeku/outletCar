b1daf2be3bc1b633cd3fd4a96c12078ba99347e9
Jorge Lekuona <jorge.lekuona@opendeusto.es>
2019-11-18 12:24:58 +0100

    a
    a

--- appOutletCar/urls.py
+++ appOutletCar/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [  
+	 path('', views.ListViewCoches.as_view(), name='list'),
 
- ]
+    path('<int:pk>/', views.DetailViewCoches.as_view(), name='detail')
+]
--- appOutletCar/views.py
+++ appOutletCar/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from .models import Marca, Modelo, Coche, Usuario, Lugar
 from .filters import FiltroCoches
 from django.views.generic import ListView, DetailView
-
+from django.views import View
 
 
 class ListViewCoches(ListView):
@@ -12,8 +12,8 @@ class ListViewCoches(ListView):
 	template_name = 'coche_lista.html'
 	def get_context_data(self, **kwargs):
 		context=super().get_context_data(**kwargs)
-		context['filter']=SnippetFilter(self.request.GET, queryset=self.get_queryset())
+		context['filter']=FiltroCoches(self.request.GET, queryset=self.get_queryset())
 		return context
-class SnippetDetailView(DetailView):
+class DetailViewCoches(DetailView):
     model = Coche
     template_name = 'coche_detalle.html'
