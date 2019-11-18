792b8d2603cd7c4925845a27ecccf8a449483809
Jorge Lekuona <jorge.lekuona@opendeusto.es>
2019-11-18 12:11:07 +0100

    a
    a

--- appOutletCar/views.py
+++ appOutletCar/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, get_list_or_404
-# Create your views here.
-
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
+from django.urls import reverse
 from .models import Marca, Modelo, Coche, Usuario, Lugar
 from .filters import FiltroCoches
 from django.views.generic import ListView, DetailView
@@ -12,8 +10,10 @@ from django.views.generic import ListView, DetailView
 class ListViewCoches(ListView):
 	model = Coche
 	template_name = 'coche_lista.html'
-
-
+	def get_context_data(self, **kwargs):
+		context=super().get_context_data(**kwargs)
+		context['filter']=SnippetFilter(self.request.GET, queryset=self.get_queryset())
+		return context
 class SnippetDetailView(DetailView):
     model = Coche
     template_name = 'coche_detalle.html'
