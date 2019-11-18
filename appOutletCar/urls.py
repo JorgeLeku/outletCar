6af055b01a270d08832551a857fb2ce94866ce3d
Jorge Lekuona <jorge.lekuona@opendeusto.es>
2019-11-18 12:49:53 +0100

    a
    a

--- appOutletCar/urls.py
+++ appOutletCar/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+app_name = 'appOutletCar'
 urlpatterns = [  
 	 path('', views.ListViewCoches.as_view(), name='list'),
-
     path('<int:pk>/', views.DetailViewCoches.as_view(), name='detail')
 ]
